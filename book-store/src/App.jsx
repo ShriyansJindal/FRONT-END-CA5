4b5b6f7c0e84fc968cd84d98b43c622bf5aa8de1
shriyans jindal <shriyansjindal40517@gmail.com>
2024-01-29 10:46:07 +0530

    home page added

---
+++ book-store/src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+import Home from './Components/Home'
+import Register from "./Components/Register"
+import { Route, Routes } from 'react-router-dom';
+
+function App() {
+
+
+  return (
+    <>
+
+      <Routes>
+      <Route  path="/" element={<Home/>}/>
+      <Route path='/register' element={<Register/>}/>
+    </Routes>
+    </>
+  )
+}
+
+export default App
---
+++ book-store/src/Components/Home.jsx
@@ -0,0 +1,53 @@
+import React, { useEffect, useState } from 'react'
+import axios from "axios"
+import "./Home.css"
+import { Link } from 'react-router-dom';
+const Home = () => {
+    let [data,setData] = useState([])
+    let [suggest, SetSuggest]=useState([])
+    let [searchitem,SetSearchitem] = useState("")
+    useEffect(()=>{
+        axios.get("https://reactnd-books-api.udacity.com/books",{headers:{ 'Authorization': 'whatever-you-want' }}).then(res=>{setData(res.data.books)
+    SetSuggest(res.data.books)}).catch(error=>{
+            console.log(error)
+        })
+    },[])
+    // console.log(data)
+    let handlechange =(e)=>{
+        SetSearchitem(e.target.value)
+        SetSuggest(data.filter((el)=>el.title.toLowerCase().includes(searchitem.toLowerCase())))
+        // console.log(suggest[0])
+        // console.log(searchitem)
+    }
+  return (
+    <div className='container'>
+        <nav className='navbar'>
+            <ul>
+                <li className='logo'>Kalvium Books</li>
+                <li><input className='search' type="text" placeholder='Search' list='books' onChange={handlechange} /><button className='searchbtn'>Search</button></li>
+                <li><Link to="./register"><button className='registerbtn'>Register</button></Link></li>
+            </ul>
+        </nav>
+
+        {/* <datalist id='books'>
+            {suggest.map((el)=>(
+                <option key={el.id} value={el.title}></option>
+            ))}
+        </datalist> */}
+        
+        <div className='books-container'>
+                {suggest.map((el)=>(
+                    <div key={el.id} className='books'>
+                        <img src={el.imageLinks.smallThumbnail} alt="" />
+                        <h3>{el.title}</h3>
+                        <p>Free</p>
+                    </div>
+                ))}
+
+        </div>
+
+    </div>
+  )
+}
+
+export default Home
